ca9d000175032743cffd6829bf62ded571c22475
Ian Fiske <ianfiske@gmail.com>
2008-10-10 22:24:00 +0000

    converted to S4 classes.

--- R/pcount.R
+++ R/pcount.R
@@ -1,27 +1,40 @@
-# fit the N-mixture point count model (Royle 2004)
-pcount <-
-function(stateformula, detformula,
-         data = list(y = y, covdata.site = covdata.site, covdata.obs = covdata.obs),
-         y, covdata.site = NULL, covdata.obs = NULL, K = NULL, mixture = "P")
-{ 
-  if ((mixture %in% c("P","NB")) == FALSE) stop("Mixture familiy not recognized. Please choose \"P\" or \"NB\".")
-  
-  arranged <- arrangeData(data)
-  cleaned <- handleNA(arranged, stateformula, detformula)
-  y <- cleaned$y
-  sitedata <- cleaned$covdata.site
-  obsdata <- cleaned$covdata.obs
+#' @include classes.R
+roxygen()
 
-  design <- getDesign(stateformula = stateformula, detformula = detformula,
-    y = y, sitedata = sitedata, obsdata = obsdata)
-  nNP <- design$nOP
-  nDP <- design$nDP
-  XDet <-design$XDet
-  XN <- design$XOcc
-  NParms <- design$occParms
-  detParms <- design$detParms
-  NParms[1] <- "lamconst"
-  detParms[1] <- "pconst"
+#' Fit the N-mixture point count model 
+#'
+#' This function fits the standard occupancy model of 
+#'
+#' @param stateformula Right-hand side formula describing covariates of abundance
+#' @param detformula Right-hand side formula describing covariates of detection
+#' @param umf an unMarkedFrame supplying data to the model.
+#' @return unMarkedFit object describing the model fit.
+#' @references (Royle 2004)
+#' @examples
+#' data(mallard)
+#' mallardUMF <- unMarkedFrame(mallard.y, siteCovs = mallard.site,
+#'                            obsCovs = mallard.obs)
+#' fm.nmx1 <- pcount(~ length + elev + forest, ~ ivel+ date + I(date^2),
+#'                   mallardUMF)
+#' @export
+pcount <-
+function(stateformula, detformula, umf, K = NULL, mixture = "P")
+{ 
+  if ((mixture %in% c("P","NB")) == FALSE)
+    stop("Mixture familiy not recognized. Please choose 'P' or 'NB'.")
+
+  umf <- handleNA(stateformula, detformula, umf)
+  designMats <- getDesign(stateformula, detformula, umf)
+  X <- designMats$X; V <- designMats$V
+  y <- umf@y
+  J <- ncol(y)
+  M <- nrow(y)
+
+  lamParms <- colnames(X)
+  detParms <- colnames(V)
+  nDP <- ncol(V)
+  nAP <- ncol(X)
+  nP <- nDP + nAP
 
   if(is.null(K)) K <- max(y, na.rm = TRUE) + 20
   if(K <= max(y, na.rm = TRUE))
@@ -32,7 +45,7 @@ function(stateformula, detformula,
   k.ik <- rep(k, M)
   k.jik <- rep(k, M*J)
 
-  nP <- nNP + nDP + ifelse(identical(mixture,"NB"),1,0)
+  nP <- nAP + nDP + ifelse(identical(mixture,"NB"),1,0)
   y.ji <- as.numeric(y)
   y.jik <- rep(y.ji, each = K + 1)
   navec <- is.na(y.jik)
@@ -40,8 +53,8 @@ function(stateformula, detformula,
 
   nll <- function(parms){
 
-    theta.i <- exp(XN %*% parms[1 : nNP])
-    p.ji <- plogis(XDet %*% parms[(nNP + 1) : (nNP + nDP)])
+    theta.i <- exp(X %*% parms[1 : nAP])
+    p.ji <- plogis(V %*% parms[(nAP + 1) : (nAP + nDP)])
     theta.ik <- rep(theta.i, each = K + 1)
     p.jik <- rep(p.ji, each = K + 1)
 
@@ -62,19 +75,27 @@ function(stateformula, detformula,
     -sum(log(dens.i))
   }
   
-  if(identical(mixture,"P")) {
-    fm <- optim(rep(0,nP), nll, method="BFGS")
-  }
-  else if (identical(mixture,"NB")){
-    fm <- optim(c(rep(0, nNP + nDP),1), nll, method="BFGS")
-  }
-  ests <- fm$par
-  fmAIC <- 2*fm$value + 2*nP
+#  if(identical(mixture,"P")) {
+    fm <- optim(rep(0,nP), nll, method="BFGS", hessian = TRUE)
+#  }
+#  else if (identical(mixture,"NB")){
+#    fm <- optim(c(rep(0, nP),1), nll, method="BFGS")
+#  }
 
+  ests <- fm$par
   if(identical(mixture,"NB"))
      {nbParm <- "alpha"}
   else
      {nbParm <- character(0)}
-  names(ests) <- c(NParms, detParms, nbParm)
-  list(estimates = ests, AIC = fmAIC)
+  names(ests) <- c(lamParms, detParms, nbParm)
+  ests.se <- diag(solve(fm$hessian))
+  fmAIC <- 2 * fm$value + 2 * nP
+  names(ests.se) <- c(lamParms, detParms)
+  umfit <- unMarkedFit(fitType = "pcount",
+                       stateformula = stateformula, detformula = detformula,
+                       data = umf, stateMLE = ests[1:nAP],
+                       stateSE = ests.se[1:nAP], 
+                       detMLE = ests[(nAP + 1) : nP],
+                       detSE = ests.se[(nAP + 1): nP], AIC = fmAIC)
+  return(umfit)
 }
