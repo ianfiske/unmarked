c116451f52479c167e1a3607169a5cfa153d9f06
Ian Fiske <ianfiske@gmail.com>
2008-10-15 20:30:21 +0000

    fixed transpose y bug and added binary truncations.

--- R/occu.R
+++ R/occu.R
@@ -1,19 +1,43 @@
 #' @include classes.R
+#' @include utils.R
 roxygen()
 
 #' Fit the MacKenzie Occupancy Model
 #'
-#' This function fits the standard occupancy model of 
+#' This function fits the standard occupancy model of MacKenzie et al
 #'
-#' @param stateformula Right-hand side formula describing covariates of abundance
-#' @param detformula Right-hand side formula describing covariates of detection
-#' @param umf unMarkedFrame supplying data to the model.
+#'  See \link{unmarked} for detailed descriptions of passing data \code{y},
+#'  \code{covdata.site}, and \code{covdata.obs}, and specifying covariates
+#'  with \code{stateformula} and \code{detformula}.
+#'
+#'  \command{occu} fits the traditional occupancy model based on the
+#'  binomial mixture models (MacKenzie et al. 2006, Royle and Dorazio
+#'  2008).  The occupancy state process (\eqn{z_i}) of site \eqn{i} is
+#'  modeled as
+#'
+#'  \deqn{z_i \sim Bernoulli(\psi_i)}{z_i ~ Bernoulli(psi_i)}
+#'
+#'  The observation process is modeled as
+#'
+#'  \deqn{y_{ij} \sim Bernoulli(p_{ij})}{y_ij ~ Bernoulli(p_ij)}
+#'
+#'  Covariates of \eqn{\psi_i}{psi_i} and \eqn{p_{ij}}{p_ij} are modelled
+#'  using the logit link.
+#'  
+#' @param stateformula right-hand side formula describing covariates of occurence.
+#' @param detformula right-hand side formula describing covariates of detection.
+#' @param umf unMarkedFrame object that supplies the data (see \link{unMarkedFrame})..
 #' @return unMarkedFit object describing the model fit.
-#' @references MacKenzie 2002, Occupancy.
+#' @references
+#' MacKenzie, D. I., J. D. Nichols, G. B. Lachman, S. Droege, J. Andrew Royle, and C. A. Langtimm. “Estimating Site Occupancy Rates When Detection Probabilities Are Less Than One.” Ecology 83, no. 8 (2002): 2248-2255.
+#' MacKenzie, D. I. et al. (2006) \emph{Occupancy Estimation and Modeling}.  Amsterdam: Academic Press.  Royle, J. A. and R. Dorazio. (2008) \emph{Book Name}.
+#' @author Ian Fiske
 #' @examples
 #' data(frogs)
 #' pcruUM <- unMarkedFrame(pcru.bin)
 #' fm <- occu(~1, ~1, pcruUM)
+#' summary(fm)
+#' @keywords models
 #' @export
 occu <-
 function(stateformula, detformula, umf)
@@ -21,7 +45,7 @@ function(stateformula, detformula, umf)
   umf <- handleNA(stateformula, detformula, umf)
   designMats <- getDesign(stateformula, detformula, umf)
   X <- designMats$X; V <- designMats$V
-  y <- umf@y
+  y <- truncateToBinary(umf@y)
   J <- ncol(y)
   M <- nrow(y)
 
@@ -31,10 +55,10 @@ function(stateformula, detformula, umf)
   nOP <- ncol(X)
 
   nP <- nDP + nOP
-  yvec <- as.numeric(y)
+  yvec <- as.numeric(t(y))
   navec <- is.na(yvec)
   nd <- ifelse(rowSums(y,na.rm=TRUE) == 0, 1, 0) # no det at site i indicator
-  
+
   nll <- function(parms) {
     psi <- plogis(X %*% parms[1 : nOP])
     pvec <- plogis(V %*% parms[(nOP + 1) : nP])
@@ -45,7 +69,7 @@ function(stateformula, detformula, umf)
     -sum(loglik)
   }
   
-  fm <- optim(rep(0, nP), nll, method = "BFGS", hessian = TRUE)
+  fm <- optim(rnorm(nP), nll, method = "BFGS", hessian = TRUE)
   ests.se <- diag(solve(fm$hessian))
   ests <- fm$par
   fmAIC <- 2 * fm$value + 2 * nP
