200c34cc07f9f29eb55e399a30d7a62003fbada8
Ian Fiske <ianfiske@gmail.com>
2008-12-12 16:20:25 +0000

    updated version no. and date.

--- R/unmarked-package.R
+++ R/unmarked-package.R
@@ -3,7 +3,7 @@
 #' \tabular{ll}{
 #' Package: \tab unmarked\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.5-1\cr
+#' Version: \tab 0.5-2\cr
 #' Date: \tab 2008-10-15\cr
 #' License: \tab GPL (>= 2)\cr
 #' LazyLoad: \tab yes\cr
@@ -25,7 +25,7 @@
 #' 2005).  All of these functions allow the user to specify covariates that
 #' affect the detection process and several also allow covariates for the
 #' state process.
-#' 
+#'
 #' \strong{Data:} All model-fitting functions analyze data that has been
 #' collected from M sites with up to J samples taken at each site, assuming
 #' closure within sites.  In general, survey
@@ -34,15 +34,15 @@
 #' \code{data}.  \code{data} is a list containing \code{y},
 #' \code{covdata.site}, and \code{covdata.obs}.  The components of
 #' \code{data} must have these names.
-#' 
-#' 
+#'
+#'
 #' \dfn{Observation} data are the observed responses, such as 0's and 1's
 #' for occurrence data or non-negative integers for point count data.
 #' Observation data is passed to unmarked functions as the \code{y} argument.
 #' For a study with \eqn{M} sites and at most \eqn{J} observations per
 #' site, \code{y} is an M x J matrix, where the \eqn{[i,j]^{th}} element is
 #' the \eqn{j^{th}} observation for the \eqn{i^{th}} site.
-#' 
+#'
 #' \dfn{Covariate} data may be specified at either the observation level or
 #' at the site level.  Observation-level covariates are passed to unmarked
 #' functions as the \code{covdata.obs} argument.  \code{covdata.obs} may
@@ -59,7 +59,7 @@
 #' site-level covariate may be represented by an M x J matrix where each
 #' column is identical.  This matrix can part of \code{covdata.obs} and
 #' appropriately named.
-#' 
+#'
 #' \strong{Model Specification:}  Most of \emph{unmarked}'s
 #' model-fitting functions allow specification of covariates for both the
 #' state process and the detection process.  Covariates for the state
@@ -71,28 +71,28 @@
 #' the process of interest.  The meaning of these covariates or, what they
 #' model, is full described in the help files for the individual functions
 #' and is not the same for all functions.
-#' 
+#'
 #' \strong{Utility Functions:}  \emph{Unmarked} contains several utility
 #' functions for massaging data into the form required by its model-fitting
 #' functions.  \command{\link{csvToUMF}} converts an appropriately
 #' formated comma-separated values (.csv) file to a list containing the
 #' components required by model-fitting functions.
-#' 
+#'
 #' @references
 #' MacKenzie, D. I. et al. (2006) \emph{Occupancy Estimation and Modeling}.
 #' Amsterdam: Academic Press.
-#'   
+#'
 #' Royle, J. A. and Nichols, J. D. (2003) Estimating Abundance from
 #' Repeated Presence-Absence Data or Point Counts. \emph{Ecology}, 84(3)
 #' pp. 777--790.
-#' 
+#'
 #' Royle, J. A. (2004) N-Mixture Models for Estimating Population Size from
 #' Spatially Replicated Counts. \emph{Biometrics} 60, pp. 108--105.
-#' 
+#'
 #' Kery, M. and Royle, J. A. (2005) Modeling Avaian Abundance from
 #' Replicated Counts Using Binomial Mixture Models. \emph{Ecological
 #'   Applications} 15(4), pp. 1450--1461.
-#' 
+#'
 #' Royle, J. A. and Link W. A. (2005) A general class of multinomial
 #' mixture models for anuran calling survey data. \emph{Ecology}, \bold{86}(9),
 #' pp. 2505--2512.
