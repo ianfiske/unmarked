51b364f2866627de1146ddb4369588ed00ba8444
Ian Fiske <ianfiske@gmail.com>
2008-12-17 02:56:56 +0000

    update version.

--- R/unmarked-package.R
+++ R/unmarked-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab unmarked\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.5-2\cr
-#' Date: \tab 2008-10-15\cr
+#' Version: \tab 0.5-3\cr
+#' Date: \tab 2008-12-16\cr
 #' License: \tab GPL (>= 2)\cr
 #' LazyLoad: \tab yes\cr
 #' LazyData: \tab yes\cr
