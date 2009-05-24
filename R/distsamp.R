c9a9b5fb1bf70632528db3d3627d3f04298ad217
Ian Fiske <ianfiske@gmail.com>
2009-05-24 15:09:04 +0000

    fixed exceptions for keyfun=="uniform" b/c there is no detection estimate in this case.  examples now run w/o errors.
    fixed file indentation.

--- R/distsamp.R
+++ R/distsamp.R
@@ -65,23 +65,23 @@ roxygen()
 #'
 #' @exportClass umDistsampFit
 setClass("umDistsampFit",
-    representation(fitType = "character",
-        call = "call",
-        stateformula = "formula",
-        detformula = "formula",
-        data = "data.frame",
-        keyfun = "character",
-        dist.breaks = "numeric",
-        tlength = "numeric",
-        area = "numeric",
-        survey = "character",
-        unitsIn = "character",
-        unitsOut = "character",
-        estimates = "unmarkedEstimateList",
-        AIC = "numeric",
-        hessian = "matrix",
-        negLogLike = "numeric")
-        )
+		representation(fitType = "character",
+				call = "call",
+				stateformula = "formula",
+				detformula = "formula",
+				data = "data.frame",
+				keyfun = "character",
+				dist.breaks = "numeric",
+				tlength = "numeric",
+				area = "numeric",
+				survey = "character",
+				unitsIn = "character",
+				unitsOut = "character",
+				estimates = "unmarkedEstimateList",
+				AIC = "numeric",
+				hessian = "matrix",
+				negLogLike = "numeric")
+)
 
 
 
@@ -158,7 +158,7 @@ setClass("umDistsampFit",
 #' exp(coef(fmhz, altNames=T))
 #' plot(function(x) unmarked:::gxhaz(x, shape=8.38, scale=1.37), 0, 25)
 #' 
-## Uniform detection function. Density output in hectars.
+#' ## Uniform detection function. Density output in hectars.
 #' (fmu <- distsamp(cbind(o1,o2,o3,o4) ~ 1, ~1, linetran, dist.breaks=dbreaksLine, 
 #' 	tlength=linetran$Length*1000, key="uniform", survey="line", unitsIn="m"))
 #' exp(coef(fmu, altNames=T))
@@ -188,147 +188,153 @@ setClass("umDistsampFit",
 #' @export
 #' @keywords models
 distsamp <- function(stateformula, detformula=~1, data, dist.breaks, 
-	tlength=NULL, keyfun=c("halfnorm", "exp", "hazard", "uniform"), 
-	survey, output=c("density", "abund"), unitsIn, unitsOut=c("ha", "kmsq"), 
-   starts=NULL, method="BFGS", control=list(), ...)
+		tlength=NULL, keyfun=c("halfnorm", "exp", "hazard", "uniform"), 
+		survey, output=c("density", "abund"), unitsIn, unitsOut=c("ha", "kmsq"), 
+		starts=NULL, method="BFGS", control=list(), ...)
 {
-keyfun <- match.arg(keyfun)
-output <- match.arg(output)
-unitsOut <- match.arg(unitsOut)
-mf <- model.frame(stateformula, data)
-Y <- model.response(mf)
-Xlam <- model.matrix(stateformula, data)
-Xp <- model.matrix(detformula, data)
-n <- nrow(Y)
-J <- ncol(Y)
-lamParms <- colnames(Xlam)
-detParms <- colnames(Xp)
-altlamParms <- paste("lam", colnames(Xlam), sep="")
-nAP <- length(lamParms)
-nDP <- length(detParms)
-nP <- nAP + nDP
-if(J != length(dist.breaks) - 1)
-	stop("ncol(response matrix) must equal length(dist.breaks)-1")
-if(dist.breaks[1] != 0)
-	stop("dist.breaks[1] must be 0")
-switch(unitsIn, 
-	km = conv <- 1,
-	m = conv <- 1000)
-if(output=="density") {
-   switch(survey, 
-      line = {
-         stripwidths <- (((dist.breaks*2)[-1] - (dist.breaks*2)[-(J+1)])) / conv
-         tl <- tlength / conv
-         a <- rep(tl, each=J) * stripwidths	 # km^2
-         },
-      point = {
-         W <- max(dist.breaks) / conv
-         a <- pi * W^2				             # km^2
-         })
-   if(unitsOut=="ha") a <- a * 100
-   }
-   else {
-      switch(survey, 
-	     line = a <- tlength / conv, # transect length must be accounted for
-	     point = a <- 1)
-      }
-if(is.null(tlength)) tlength <- numeric(0)
-switch(keyfun, 
-halfnorm = { 
-      altdetParms <- paste("sigma", colnames(Xp), sep="")
-      if(is.null(starts)) {
-      starts <- c(rep(0, nAP), log(max(dist.breaks)), rep(0, nDP-1))
-      names(starts) <- c(lamParms, detParms)
-      }
-   else {
-      if(is.null(names(starts))) names(starts) <- c(lamParms, detParms)
-      }
-   if(!all(names(starts) %in% c(lamParms, detParms)))
-      stop("names(starts) does not agree with necessary model parameters")
-   fm <- optim(starts, ll.halfnorm, Y=Y, Xlam=Xlam, Xp=Xp, J=J, a=a, 
-      d=dist.breaks, nAP=nAP, nP=nP, survey=survey, method=method, hessian=T,
-      control=control, ...)
-   },
-exp = { 
-      altdetParms <- paste("rate", colnames(Xp), sep="")
-      if(is.null(starts)) {
-      starts <- c(rep(0, nAP), log(median(dist.breaks)), rep(0, nDP-1))
-      names(starts) <- c(lamParms, detParms)
-      }
-   else {
-	if(is.null(names(starts)))
-		names(starts) <- c(lamParms, detParms)
-      }
-   if(!all(names(starts) %in% c(lamParms, detParms)))
-      stop("names(starts) does not agree with necessary model parameters")
-   fm <- optim(starts, ll.exp, Y=Y, Xlam=Xlam, Xp=Xp, J=J, d=dist.breaks, 
-      a=a, nAP=nAP, nP=nP, survey=survey, method=method, hessian=T, 
-      control=control, ...)
-	  },
-hazard = {	
-   detParms <- c(detParms, "scale")
-   nDP <- length(detParms)
-   nP <- nAP + nDP
-   altdetParms <- c(paste("shape", colnames(Xp), sep=""), "scale")
-   if(is.null(starts)) {
-      starts <- c(rep(0, nAP), log(median(dist.breaks)), rep(0, nDP-2), 1)
-      names(starts) <- c(lamParms, detParms)
+	keyfun <- match.arg(keyfun)
+	output <- match.arg(output)
+	unitsOut <- match.arg(unitsOut)
+	mf <- model.frame(stateformula, data)
+	Y <- model.response(mf)
+	Xlam <- model.matrix(stateformula, data)
+	Xp <- model.matrix(detformula, data)
+	n <- nrow(Y)
+	J <- ncol(Y)
+	lamParms <- colnames(Xlam)
+	detParms <- colnames(Xp)
+	altlamParms <- paste("lam", colnames(Xlam), sep="")
+	nAP <- length(lamParms)
+	nDP <- length(detParms)
+	nP <- nAP + nDP
+	if(J != length(dist.breaks) - 1)
+		stop("ncol(response matrix) must equal length(dist.breaks)-1")
+	if(dist.breaks[1] != 0)
+		stop("dist.breaks[1] must be 0")
+	switch(unitsIn, 
+			km = conv <- 1,
+			m = conv <- 1000)
+	if(output=="density") {
+		switch(survey, 
+				line = {
+					stripwidths <- (((dist.breaks*2)[-1] - (dist.breaks*2)[-(J+1)])) / conv
+					tl <- tlength / conv
+					a <- rep(tl, each=J) * stripwidths	 # km^2
+				},
+				point = {
+					W <- max(dist.breaks) / conv
+					a <- pi * W^2				             # km^2
+				})
+		if(unitsOut=="ha") a <- a * 100
 	}
-   else {
-	if(is.null(names(starts)))
-		names(starts) <- c(lamParms, detParms)
+	else {
+		switch(survey, 
+				line = a <- tlength / conv, # transect length must be accounted for
+				point = a <- 1)
 	}
-   if(!all(names(starts) %in% c(lamParms, detParms)))
-      stop("names(starts) does not agree with necessary model parameters")
-   fm <- optim(starts, ll.hazard, Y=Y, Xlam=Xlam, Xp=Xp, J=J, d=dist.breaks, 
-      a=a, nAP=nAP, nP=nP, survey=survey, method=method, hessian=T, 
-      control=control, ...)
-   }, 
-uniform = {
-   detParms <- character(0)
-   altdetParms <- character(0)
-   nDP <- 0	
-   if(is.null(starts)) {
-      starts <- rep(0, length(lamParms))
-      names(starts) <- lamParms
-      }
-   else {
-	if(is.null(names(starts)))
-		names(starts) <- lamParms
-      }
-   if(!all(names(starts) %in% lamParms))
-      stop("names(starts) does not agree with necessary model parameters")
-   fm <- optim(starts, ll.uniform, Y=Y, Xlam=Xlam, Xp=Xp, J=J, a=a, 
-      method=method, hessian=T, control=control, ...)
-   })
-ests <- fm$par
-estsAP <- ests[1:nAP]
-estsDP <- ests[(nAP+1):nP]
-attr(estsAP, "altNames") <- altlamParms
-attr(estsDP, "altNames") <- altdetParms
-try(covMat <- solve(fm$hessian))
-covMatAP <- as.matrix(covMat[1:nAP, 1:nAP])
-if(keyfun=="uniform")
-   covMatDP <- matrix(numeric(0), 1)
-else
-   covMatDP <- as.matrix(covMat[(nAP+1):nP, (nAP+1):nP])
-attr(covMatAP, "altNames") <- list(altlamParms, altlamParms)
-attr(covMatDP, "altNames") <- list(altdetParms, altdetParms)
-attr(fm$hessian, "altNames") <- list(c(altlamParms, altdetParms), 
-   c(altlamParms, altdetParms))
-fmAIC <- 2 * fm$value + 2 * nP
-stateEstimates <- unmarkedEstimate(name = "Abundance", estimates = estsAP,
-   covMat = covMatAP, invlink = "exp", invlinkGrad = "exp")
-detEstimates <- unmarkedEstimate(name = "Detection", estimates = estsDP, 
-   covMat = covMatDP, invlink = "exp", invlinkGrad = "exp")
-estimateList <- unmarkedEstimateList(list(state=stateEstimates, 
-   det=detEstimates))
-dsfit <- new("umDistsampFit", fitType = "distsamp", call = match.call(), 
-   stateformula=stateformula, detformula=detformula, data = data, keyfun=keyfun, 
-   dist.breaks=dist.breaks, tlength=tlength, area=a, survey=survey, 
-   unitsIn=unitsIn, unitsOut=unitsOut, estimates = estimateList, AIC = fmAIC, 
-   hessian = fm$hessian, negLogLike = fm$value)
-return(dsfit)
+	if(is.null(tlength)) tlength <- numeric(0)
+	switch(keyfun, 
+			halfnorm = { 
+				altdetParms <- paste("sigma", colnames(Xp), sep="")
+				if(is.null(starts)) {
+					starts <- c(rep(0, nAP), log(max(dist.breaks)), rep(0, nDP-1))
+					names(starts) <- c(lamParms, detParms)
+				}
+				else {
+					if(is.null(names(starts))) names(starts) <- c(lamParms, detParms)
+				}
+				if(!all(names(starts) %in% c(lamParms, detParms)))
+					stop("names(starts) does not agree with necessary model parameters")
+				fm <- optim(starts, ll.halfnorm, Y=Y, Xlam=Xlam, Xp=Xp, J=J, a=a, 
+						d=dist.breaks, nAP=nAP, nP=nP, survey=survey, method=method, hessian=T,
+						control=control, ...)
+			},
+			exp = { 
+				altdetParms <- paste("rate", colnames(Xp), sep="")
+				if(is.null(starts)) {
+					starts <- c(rep(0, nAP), log(median(dist.breaks)), rep(0, nDP-1))
+					names(starts) <- c(lamParms, detParms)
+				}
+				else {
+					if(is.null(names(starts)))
+						names(starts) <- c(lamParms, detParms)
+				}
+				if(!all(names(starts) %in% c(lamParms, detParms)))
+					stop("names(starts) does not agree with necessary model parameters")
+				fm <- optim(starts, ll.exp, Y=Y, Xlam=Xlam, Xp=Xp, J=J, d=dist.breaks, 
+						a=a, nAP=nAP, nP=nP, survey=survey, method=method, hessian=T, 
+						control=control, ...)
+			},
+			hazard = {	
+				detParms <- c(detParms, "scale")
+				nDP <- length(detParms)
+				nP <- nAP + nDP
+				altdetParms <- c(paste("shape", colnames(Xp), sep=""), "scale")
+				if(is.null(starts)) {
+					starts <- c(rep(0, nAP), log(median(dist.breaks)), rep(0, nDP-2), 1)
+					names(starts) <- c(lamParms, detParms)
+				}
+				else {
+					if(is.null(names(starts)))
+						names(starts) <- c(lamParms, detParms)
+				}
+				if(!all(names(starts) %in% c(lamParms, detParms)))
+					stop("names(starts) does not agree with necessary model parameters")
+				fm <- optim(starts, ll.hazard, Y=Y, Xlam=Xlam, Xp=Xp, J=J, d=dist.breaks, 
+						a=a, nAP=nAP, nP=nP, survey=survey, method=method, hessian=T, 
+						control=control, ...)
+			}, 
+			uniform = {
+				detParms <- character(0)
+				altdetParms <- character(0)
+				nDP <- 0	
+				if(is.null(starts)) {
+					starts <- rep(0, length(lamParms))
+					names(starts) <- lamParms
+				}
+				else {
+					if(is.null(names(starts)))
+						names(starts) <- lamParms
+				}
+				if(!all(names(starts) %in% lamParms))
+					stop("names(starts) does not agree with necessary model parameters")
+				fm <- optim(starts, ll.uniform, Y=Y, Xlam=Xlam, Xp=Xp, J=J, a=a, 
+						method=method, hessian=T, control=control, ...)
+			})
+	ests <- fm$par
+	estsAP <- ests[1:nAP]
+	estsDP <- ests[(nAP+1):nP]
+	attr(estsAP, "altNames") <- altlamParms
+	attr(estsDP, "altNames") <- altdetParms
+	try(covMat <- solve(fm$hessian))
+	covMatAP <- as.matrix(covMat[1:nAP, 1:nAP])
+	if(keyfun=="uniform")
+		covMatDP <- matrix(numeric(0), 1)
+	else
+		covMatDP <- as.matrix(covMat[(nAP+1):nP, (nAP+1):nP])
+	attr(covMatAP, "altNames") <- list(altlamParms, altlamParms)
+	attr(covMatDP, "altNames") <- list(altdetParms, altdetParms)
+	attr(fm$hessian, "altNames") <- list(c(altlamParms, altdetParms), 
+			c(altlamParms, altdetParms))
+	fmAIC <- 2 * fm$value + 2 * nP
+	if (keyfun != "uniform") {
+		stateEstimates <- unmarkedEstimate(name = "Abundance", estimates = estsAP,
+				covMat = covMatAP, invlink = "exp", invlinkGrad = "exp")
+		detEstimates <- unmarkedEstimate(name = "Detection", estimates = estsDP, 
+				covMat = covMatDP, invlink = "exp", invlinkGrad = "exp")
+		estimateList <- unmarkedEstimateList(list(state=stateEstimates, 
+						det=detEstimates))
+	} else {
+		stateEstimates <- unmarkedEstimate(name = "Abundance", estimates = estsAP,
+				covMat = covMatAP, invlink = "exp", invlinkGrad = "exp")
+		estimateList <- unmarkedEstimateList(list(state=stateEstimates))
+	}
+	dsfit <- new("umDistsampFit", fitType = "distsamp", call = match.call(), 
+			stateformula=stateformula, detformula=detformula, data = data, keyfun=keyfun, 
+			dist.breaks=dist.breaks, tlength=tlength, area=a, survey=survey, 
+			unitsIn=unitsIn, unitsOut=unitsOut, estimates = estimateList, AIC = fmAIC, 
+			hessian = fm$hessian, negLogLike = fm$value)
+	return(dsfit)
 }
 
 
@@ -341,15 +347,15 @@ return(dsfit)
 
 #' @exportMethod show
 setMethod("show", "umDistsampFit", function(object)
-{
-cat("\n", "Call: ", deparse(object@call), "\n", sep="", fill=T)
-cat("Coefficients:\n")
-show(object@estimates)
-cat("Key-function:", object@keyfun)
+		{
+			cat("\n", "Call: ", deparse(object@call), "\n", sep="", fill=T)
+			cat("Coefficients:\n")
+			show(object@estimates)
+			cat("Key-function:", object@keyfun)
 #cat("\nOutput:", object@output, "\n")
-cat("\nAIC:", object@AIC)
-cat("\nSample size: ", nrow(object@data), "\n")
-})
+			cat("\nAIC:", object@AIC)
+			cat("\nSample size: ", nrow(object@data), "\n")
+		})
 
 #
 #setGeneric("coef",
@@ -362,41 +368,41 @@ cat("\nSample size: ", nrow(object@data), "\n")
 
 #' @exportMethod coef
 setMethod("coef", "umDistsampFit", function(object, type=NULL, altNames=F)
-{
-if(is.null(type)) {
-   eap <- object@estimates["state"]@estimates
-   edp <- object@estimates["det"]@estimates
-   e <- c(eap, edp)
-   if(altNames)
-      names(e) <- c(attr(eap, "altNames"), attr(edp, "altNames"))
-   } 
-else {
-   e <- object@estimates[type]@estimates
-   if(altNames)
-      names(e) <- attr(e, "altNames")
-   }
-attr(e, "altNames") <- NULL
-return(e)
-})
+		{
+			if(is.null(type)) {
+				eap <- object@estimates["state"]@estimates
+				edp <- NULL
+				if(object@keyfun != "uniform") edp <- object@estimates["det"]@estimates
+				e <- c(eap, edp)
+				if(altNames)
+					names(e) <- c(attr(eap, "altNames"), attr(edp, "altNames"))
+			}	else {
+				e <- object@estimates[type]@estimates
+				if(altNames)
+					names(e) <- attr(e, "altNames")
+			}
+			attr(e, "altNames") <- NULL
+			return(e)
+		})
 
 
 #' @exportMethod vcov
 setMethod("vcov", "umDistsampFit", function(object, type=NULL, drop=F, 
-   altNames=F)
-{
-if(is.null(type)) {
-   vc <- solve(object@hessian)
-   if(altNames)
-      dimnames(vc) <- attr(object@hessian, "altNames")
-   } 
-else {
-   if(!is.null(type)) {
-   vc <- object@estimates[type]@covMat
-   if(altNames)
-      dimnames(vc) <- attr(vc, "altNames")
-      }
-   }
-attr(vc, "altNames") <- NULL
-return(vc)
-})      
+				altNames=F)
+		{
+			if(is.null(type)) {
+				vc <- solve(object@hessian)
+				if(altNames)
+					dimnames(vc) <- attr(object@hessian, "altNames")
+			} 
+			else {
+				if(!is.null(type)) {
+					vc <- object@estimates[type]@covMat
+					if(altNames)
+						dimnames(vc) <- attr(vc, "altNames")
+				}
+			}
+			attr(vc, "altNames") <- NULL
+			return(vc)
+		})      
 
