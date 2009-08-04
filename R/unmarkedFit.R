169b69dbfcb06afcf7dc465c9a49c2adc903be8f
Richard Chandler <rchandler@nrc.umass.edu>
2009-08-04 16:40:05 +0000

    Fixed update method for unmarkedFit objects. New formulas can now be specified.

--- R/unmarkedFit.R
+++ R/unmarkedFit.R
@@ -324,26 +324,35 @@ setMethod("hessian", "unmarkedFit",
 
 #' @exportMethod update
 setMethod("update", "unmarkedFit", 
-		function(object, formula., ..., evaluate = TRUE) {
-			call <- object@call
-			if (is.null(call)) 
-				stop("need an object with call slot")
-			extras <- match.call(expand.dots = FALSE)$...
-			if (!missing(formula.)) 
-				call$formula <- update.formula(formula(object), formula.)
-			if (length(extras) > 0) {
-				existing <- !is.na(match(names(extras), names(call)))
-				for (a in names(extras)[existing]) call[[a]] <- extras[[a]]
-				if (any(!existing)) {
-					call <- c(as.list(call), extras[!existing])
-					call <- as.call(call)
-				}
-			}
-			if (evaluate) 
-				eval(call, parent.frame())
-			else call
+	function(object, formula., ..., evaluate = TRUE) 
+{
+	call <- object@call
+	origFormula <- formula(call)
+	if (is.null(call)) 
+		stop("need an object with call slot")
+	extras <- match.call(expand.dots = FALSE)$...
+	if (!missing(formula.)) {
+		detformula <- as.formula(origFormula[[2]])
+		stateformula <- as.formula(paste("~", origFormula[3], sep=""))
+		newStateformula <- as.formula(formula.[[2]])
+		newDetformula <- as.formula(paste("~", formula.[3], sep=""))
+		upStateformula <- update.formula(stateformula, newStateformula)
+		upDetformula <- update.formula(detformula, newDetformula)
+		call$formula <- as.formula(paste(deparse(upDetformula), 
+			deparse(upStateformula)))
 		}
-)
+	if (length(extras) > 0) {
+		existing <- !is.na(match(names(extras), names(call)))
+		for (a in names(extras)[existing]) call[[a]] <- extras[[a]]
+			if (any(!existing)) {
+				call <- c(as.list(call), extras[!existing])
+				call <- as.call(call)
+				}
+		}
+	if (evaluate) 
+		eval(call, parent.frame())
+	else call
+})
 
 
 setGeneric("sampleSize", function(object) standardGeneric("sampleSize"))
