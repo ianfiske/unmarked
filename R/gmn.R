6db4308fa5d0c0f12f219d0d601715cfb4e5d26a
rbchan <richard.chandlers@gmail.com>
2010-09-09 14:01:43 -0400

    gmn is in place but getDesign and handleNA methods are not

--- R/gmn.R
+++ R/gmn.R
@@ -8,12 +8,16 @@ if(!is(data, "unmarkedMultFrame"))
 
 mixture <- match.arg(mixture)
 k <- 0:K
+lk <- length(k)
 
-D <- getDesign(data, formula)
+formula <- list(lambdaformula = lambdaformula, phiformula = phiformula, 
+    pformula = pformula)
+D <- getDesign(data, formula = as.formula(paste(unlist(formula), collapse=" ")))
 Xlam <- D$Xlam
 Xphi <- D$Xphi 
 Xdet <- D$Xdet
-y <- D$y
+y <- D$y  # MxJxL
+n <- apply(y, 1:2, sum)
 
 Xlam.offset <- D$X.offset
 Xphi.offset <- D$Xphi.offset
@@ -21,61 +25,89 @@ Xdet.offset <- D$Xdet.offset
 if(is.null(Xlam.offset)) Xlam.offset <- rep(0, nrow(Xlam))
 if(is.null(Xphi.offset)) Xphi.offset <- rep(0, nrow(Xphi))
 if(is.null(Xdet.offset)) Xdet.offset <- rep(0, nrow(Xdet))
-  
+
+M <- nrow(y)  
 J <- ncol(y)
+L <- dim(y)[3]
 R <- obsNum(data)
-M <- nrow(y)
+
 piFun <- data@piFun
 
-lamParms <- colnames(Xlam)
-phiParms <- colnames(Xphi)
-detParms <- colnames(Xdet)
+lamPars <- colnames(Xlam)
+phiPars <- colnames(Xphi)
+detPars <- colnames(Xdet)
 nLP <- ncol(Xlam)
 nPP <- ncol(Xphi)
 nDP <- ncol(Xdet)
 nP <- nLP + nPP + nDP + ifelse(mixture=='NB', 1, 0)
 
-# y should probably be an MxRxJ array 
-yvec <- as.numeric(y)
-navec <- is.na(yvec)
+p <- array(as.numeric(NA), c(M, J, L))
+pi <- array(as.numeric(NA), c(M, J, R))   # L does not necessarily equal R
+lfac.k <- lgamma(k+1)
+A <- matrix(0, M, lk)
+g <- matrix(as.numeric(NA), M, lk)
 
-# n.mr should be an MxR matrix of counts
-n.mr <- apply(y.mrj, 1:2, sum)
-
-ldm <- function(x, sx, pr, kvec) {
-    
-    lgamma(kvec+1) - lgamma(kvec-sx+1) + sum(x*log(pr[1:3])) + (kvec-sx)*log(pr[4])
-    
-    }
-
-
-nll <- function(pars) 
-{
+nll <- function(pars) {
 		lambda <- exp(Xlam %*% pars[1:nLP] + Xlam.offset) 
     phi <- plogis(Xphi %*% pars[(nLP+1):(nLP+nPP)] + Xphi.offset)
-    p <- plogis(Xdet %*% pars[(nLP+nPP+1):(nLP+nPP+nDP)] + Xdet.offset)
-    p.array <- array(p, c(M, R, J))
+    p[] <- plogis(Xdet %*% pars[(nLP+nPP+1):(nLP+nPP+nDP)] + Xdet.offset)
+    cp.mat <- apply(p, 2, function(x) do.call(piFun, list(x))) * phi
+    cp[] <- cbind(cp.mat, 1-rowSums(pi.mat))
+    switch(mixture, 
+        P = f <- sapply(k, function(x) dpois(x, lambda)),
+        NB = f <- sapply(k, dnbinom(x, mu=lambda, size=pars[nP]))
+        )
+    for(i in 1:M) {
+        for(j in 1:J) {
+            kmn <- k-n[j]
+            kmn <- kmn[kmn >= 0]
+            A[,j] <- lfac.k - lgamma(kmn+1) + sum(y[i,j,]*log(cp[i,j,1:L])) + 
+                kmn*log(cp[i,j,L])
+            }
+        g[i,] <- exp(rowSums(A))
+        }
+    ll <- rowSums(f*g)
+    -sum(log(ll))
+    }
     
-    f <- sapply(lambda, function(x) dpois(k, lambda))
+if(missing(starts)) starts <- rep(0, nP)
+fm <- optim(starts, nll, method = method, hessian = se, control = control)
+opt <- fm
+if(se) {
+		covMat <- tryCatch(solve(fm$hessian), error=function(x) 
+        simpleError("Hessian is singular. Try using fewer covariates."))
+    if(identical(class(covMat)[1], "simpleError")) {
+        warning(covMat$message)
+        covMat <- matrix(NA, nP, nP)
+        }
+    } else covMat <- matrix(NA, nP, nP)
+ests <- fm$par
+fmAIC <- 2 * fm$value + 2 * nP
+names(ests) <- c(lamPars, phiPars, detPars)
 
-    for(r in 1:R) {
+lamEstimates <- unmarkedEstimate(name = "Abundance", short.name = "lambda",
+    estimates = ests[1:nAP],
+		covMat = as.matrix(covMat[1:nAP, 1:nAP]), invlink = "exp",
+		invlinkGrad = "exp")
+phiEstimates <- unmarkedEstimate(name = "Availability", short.name = "phi",
+    estimates = ests[(nLP+1):(nLP+nPP)],
+		covMat = as.matrix(covMat[(nLP+1):(nLP+nPP)]), invlink = "exp",
+		invlinkGrad = "exp")
+detEstimates <- unmarkedEstimate(name = "Detection", short.name = "p",
+		estimates = ests[(nPP+1):(nLP+nPP+nDP)],
+		covMat = as.matrix(covMat[(nPP+1):(nLP+nPP+nDP), (nPP+1):(nLP+nPP+nDP)]), 
+		invlink = "logistic", invlinkGrad = "logistic.grad")
+estimateList <- unmarkedEstimateList(list(lambda=stateEstimates,
+		phi=phiEstimates, det=detEstimates))
 
-p2min <- 1-(1-p1)^2
-p3min <- 1-(1-p1)^3
-p5min <- 1-(1-p1)^5
+umfit <- new("unmarkedFitGMN", fitType = "gmn", 
+		call = match.call(), formula = formula, data = data, 
+		estimates = estimateList, sitesRemoved = D$removed.sites, 
+		AIC = fmAIC, opt = opt, negLogLike = fm$value, nllFun = nll)
 
-# Cell probabilities
-cp <- c(phi*p5min, phi*(1-p5min)*p3min, phi*(1-p5min)*(1-p3min)*p2min)
-cp <- c(cp, 1-sum(cp))
-
-a1 <- ldm(data[i, 1:3], sx[1], cp, k)
-a2 <- ldm(data[i, 4:6], sx[2], cp, k)
-a3 <- ldm(data[i, 7:9], sx[3], cp, k)
-A <- cbind(a1,a2,a3)
-
-ll1 <- exp(rowSums(A))        
-    
-    ll <- rowSums(ll1 * f)
-    -sum(log(ll))  
+return(umfit)
 }
+
+
+
 
