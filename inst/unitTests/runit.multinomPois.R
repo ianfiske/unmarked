929f208d946b78c2b21831537fe6cdc88afef639
rbchan <richard.chandlers@gmail.com>
2010-11-10 08:48:23 -0500

    Added some simulation test files. These are not meant to be run as part of standard test suite

--- inst/unitTests/runit.multinomPois.R
+++ inst/unitTests/runit.multinomPois.R
@@ -21,7 +21,6 @@ test.multinomPois.na <- function() {
 
     checkEquals(obsToY(umf1), diag(3))
 
-
     m1 <- multinomPois(~1 ~1, umf1)
     checkEqualsNumeric(coef(m1), c(1.7490576,  -0.2330901), tol=1e-5)
 
@@ -29,6 +28,17 @@ test.multinomPois.na <- function() {
     m2 <- multinomPois(~x2 ~1, umf1)
     checkEqualsNumeric(coef(m2), c(1.9159845, 0.2248897, -0.1808144), tol=1e-5)
     checkEquals(m2@sitesRemoved, 4)
+    
+    
+    # The old mapping matrix    
+    o2y <- diag(3)
+    o2y[upper.tri(o2y)] <- 1
+    umf1@obsToY <- o2y
+    
+    m3 <- multinomPois(~x2 ~1, umf1)
+    checkEqualsNumeric(m3@sitesRemoved, 4)
+    
+
     
     }
 
---
+++ inst/unitTests/sim.distsamp.R
@@ -0,0 +1,56 @@
+
+
+
+
+# Simulate with half-normal detection function
+
+simDSpt <- function(lambda=5, sigma=20, nts=100, radius=50,
+    breaks=seq(0, 50, by=10))
+{      
+    A <- (2*radius)^2 / 10000 # Area (ha) of square containing circle
+    y <- matrix(0, npts, length(breaks)-1)
+    for(i in 1:npts) {
+        M <- rpois(1, lambda * A) # Individuals within the rectangle
+        # coordinates of each individual
+        xy <- cbind(x=runif(M, -radius, radius), y=runif(M, -radius, radius))
+        
+        # Distances from each point
+        d <- apply(xy, 1, function(x) sqrt(x[1]^2 + x[2]^2))
+        d <- d[d <= radius]
+
+        # Detection process
+        if(length(d)) {
+            p <- exp(-d^2 / (2 * sigma^2)) # half-normal
+            d <- d[rbinom(length(d), 1, p) == 1]
+            y[i,] <- table(cut(d, breaks, include.lowest=TRUE))
+            }
+        }
+    return(y)
+}
+
+colSums(simDSpt())
+
+set.seed(3)
+umf1 <- unmarkedFrameDS(y = simDSpt(), survey="point", 
+    dist.breaks=seq(0, 50, by=10), unitsIn="m")
+(m1 <- distsamp(~1 ~1, umf1, starts=c(log(5), log(20))))
+m2 <- distsamp(~1 ~1, umf1, starts=c(log(5), log(20)), output="abund")
+
+
+checkEqualsNumeric(coef(m1), c(1.813819, 2.893771), tol=1e-5)
+checkEquals(exp(coef(m1, type="state")), 
+    exp(coef(m2, type="state")) / (pi * 50^2 / 10000), tol=0.01)
+
+
+nsims <- 10
+simout1 <- matrix(NA, nsims, 2)
+for(i in 1:nsims) {
+    cat("sim", i, "\n"); flush.console()
+    umf <- unmarkedFrameDS(y = simDSpt(), survey="point", 
+        dist.breaks=seq(0, 50, by=10), unitsIn="m")
+    m <- distsamp(~1 ~1, umf, starts=c(log(5), log(20)))
+    simout1[i,] <- c(exp(coef(m, type="state")), exp(coef(m, type="det")))
+    }
+
+hist(simout1[,1]); abline(v=5, lwd=2, col=3)
+hist(simout1[,2]); abline(v=20, lwd=2, col=3)
