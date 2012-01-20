8aa28a3a1325bd81ff235b04d59f4ed415d839eb
Richard Chandler <richard.chandlers@gmail.com>
2012-01-20 08:48:09 -0500

    started a vignette on capture-recapture analysis

---
+++ inst/doc/auto/multinom.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook "multinom"
+ (lambda ()
+    (TeX-run-style-hooks
+     "fullpage"
+     "natbib"
+     "Sweave"
+     "fontenc"
+     "OT1"
+     "latex2e"
+     "art10"
+     "article"
+     "a4paper")))
+
