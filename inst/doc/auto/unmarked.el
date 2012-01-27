5beda9dea4742e2f695a247fe943f007fd1117f9
Richard Chandler <richard.chandlers@gmail.com>
2012-01-27 17:10:09 -0500

    vignette work

---
+++ inst/doc/auto/unmarked.el
@@ -0,0 +1,17 @@
+(TeX-add-style-hook "unmarked"
+ (lambda ()
+    (LaTeX-add-bibitems
+     "buckland_distsamp_2001"
+     "chandlerEA_2011"
+     "fiskeChandler_2011"
+     "royle_general_2005"
+     "nichols_double-observer_2000"
+     "royle_modeling_2004"
+     "royle_estimating_2003"
+     "kery_modeling_2005"
+     "royle_n-mixture_2004"
+     "mackenzie_estimating_2002"
+     "royle_generalized_2004"
+     "mackenzie_estimating_2003"
+     "williams_etal:2002")))
+
