1a157e6a74e4e0e4fea87bfbc094802597689ccd
Richard Chandler <richard.chandlers@gmail.com>
2012-03-02 10:47:53 -0500

    no fix yet

--- src/nll_distsamp.cpp
+++ src/nll_distsamp.cpp
@@ -3,6 +3,7 @@
 
 using namespace Rcpp ;
 
+
 SEXP nll_distsamp( SEXP y_, SEXP X_, SEXP V_, SEXP beta_lam_, SEXP beta_sig_, SEXP X_offset_, SEXP V_offset_, SEXP A_, SEXP a_, SEXP u_, SEXP output_, SEXP db_ ) {
 
   arma::imat y = as<arma::imat>(y_);
