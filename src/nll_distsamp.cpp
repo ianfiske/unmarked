a3b2312b1eef3e6b09832b16ec4ab5edb80b0697
Richard Chandler <richard.chandlers@gmail.com>
2012-10-17 17:13:34 -0400

    added analytical integration for negexp det fun for line transects

--- src/nll_distsamp.cpp
+++ src/nll_distsamp.cpp
@@ -39,30 +39,32 @@ SEXP nll_distsamp( SEXP y_, SEXP lam_, SEXP sig_, SEXP scale_, SEXP a_, SEXP u_,
   for(int i=0; i<R; i++) {
     if((survey=="line") & (keyfun=="halfnorm"))
       f0 = Rf_dnorm4(0.0, 0.0, sig[i], false);
+    if((survey=="line") & (keyfun=="exp"))
+      f0 = Rf_dexp(0.0, 1/sig[i], false);
     for(int j=0; j<J; j++) {
       cp = 0.0;
       ex[0] = sig[i];
       ex[1] = scale;
+      // Integration settings given to Rdqags
+      lower = db[j];
+      upper = db[j+1];
+      epsrel = Rcpp::as<double>(reltol_);
+      epsabs = epsrel;
+      limit = 100;
+      lenw = 400;
+      last = 0;
+      iwork = 100;
+      work = 400.0;
+      result = DOUBLE_XMIN;
+      abserr = 0.0;
+      neval = 0;
+      ier = 0;
       if(keyfun=="uniform") {
 	cp = u(i,j);
       } else {
-	// Integration settings given to Rdqags
-	lower = db[j];
-	upper = db[j+1];
-	epsrel = Rcpp::as<double>(reltol_);
-	epsabs = epsrel;
-	limit = 100;
-	lenw = 400;
-	last = 0;
-	iwork = 100;
-	work = 400.0;
-	result = DOUBLE_XMIN;
-	abserr = 0.0;
-	neval = 0;
-	ier = 0;
 	if(survey=="point") {
 	  if(keyfun=="halfnorm") {
-	    result = sig[i]*sig[i]*(1-exp(-upper*upper/(2*sig[i]*sig[i]))) -
+	    result = sig[i]*sig[i]*(1-exp(-upper*upper/(2*sig[i]*sig[i])))-
 	      sig[i]*sig[i]*(1-exp(-lower*lower/(2*sig[i]*sig[i])));
 	    // Rdqags(grhn, ex, &lower, &upper, &epsabs, &epsrel, &result,
 	    // 	   &abserr, &neval, &ier, &limit, &lenw, &last, &iwork,
@@ -84,13 +86,15 @@ SEXP nll_distsamp( SEXP y_, SEXP lam_, SEXP sig_, SEXP scale_, SEXP a_, SEXP u_,
 	  if(keyfun=="halfnorm") {
 	    result = (Rf_pnorm5(upper, 0.0, sig[i], true, false) -
 		      Rf_pnorm5(lower, 0.0, sig[i], true, false)) / f0;
-	    //	    Rdqags(gxhn, ex, &lower, &upper, &epsabs, &epsrel, &result,
-	    //		   &abserr, &neval, &ier, &limit, &lenw, &last, &iwork,
-	    //		   &work);
+	    // Rdqags(gxhn, ex, &lower, &upper, &epsabs, &epsrel, &result,
+	    //     &abserr, &neval, &ier, &limit, &lenw, &last, &iwork,
+	    //	   &work);
 	  } else if(keyfun=="exp") {
-	    Rdqags(gxexp, ex, &lower, &upper, &epsabs, &epsrel, &result,
-		   &abserr, &neval, &ier, &limit, &lenw, &last, &iwork,
-		   &work);
+	    result = (Rf_pexp(upper, 1/sig[i], true, false) -
+		      Rf_pexp(lower, 1/sig[i], true, false)) / f0;
+	    // Rdqags(gxexp, ex, &lower, &upper, &epsabs, &epsrel, &result,
+	    // 	   &abserr, &neval, &ier, &limit, &lenw, &last, &iwork,
+	    // 	   &work);
 	  } else if(keyfun=="hazard") {
 	    Rdqags(gxhaz, ex, &lower, &upper, &epsabs, &epsrel, &result,
 		   &abserr, &neval, &ier, &limit, &lenw, &last, &iwork,
