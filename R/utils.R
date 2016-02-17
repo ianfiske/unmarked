0cbd821f5fd017400cac43ff52e21c749351032e
aurielfournier <aurielfournier@gmail.com>
2016-02-17 16:42:16 -0600

    rm formatDistData.R replace in utils.R

--- R/utils.R
+++ R/utils.R
@@ -548,56 +548,69 @@ as.numeric(1 - exp(-lambda))
 # Convert individual-level distance data to the
 # transect-level format required by distsamp()
 
-formatDistData <- function(distData, distCol, transectNameCol, dist.breaks,
-                           occasionCol)
+formatDistData <- function (distData, distCol, transectNameCol, dist.breaks, occasionCol,effortMatrix) 
 {
-    if(!is.numeric(distData[,distCol]))
-        stop("The distances must be numeric")
-    transects <- distData[,transectNameCol]
-    if(!is.factor(transects)) {
-        transects <- as.factor(transects)
-        warning("The transects were converted to a factor")
+  if (!is.numeric(distData[, distCol])) 
+    stop("The distances must be numeric")
+  transects <- distData[, transectNameCol]
+  if (!is.factor(transects)) {
+    transects <- as.factor(transects)
+    warning("The transects were converted to a factor")
+  }
+  
+  if (missing(occasionCol)) {
+    T <- 1
+    occasions <- factor(rep(1, nrow(distData)))
+  }
+  else {
+    occasions <- distData[, occasionCol]
+    if (!is.factor(occasions)) {
+      occasions <- as.factor(occasions)
+      warning("The occasions were converted to a factor")
     }
-    if(missing(occasionCol)) {
-        T <- 1
-        occasions <- factor(rep(1, nrow(distData)))
+    T <- nlevels(occasions)
+  }
+  M <- nlevels(transects)
+  J <- length(dist.breaks) - 1
+  if (missing(effortMatrix)) {
+    effortMatrix <- matrix(nrow=M,ncol=T,1)
+  }
+  
+  if (!is.numeric(effortMatrix)){
+    stop("effortMatrix is not numeric")
+    effortMatrix <- matrix(nrow=M,ncol=T,1)
+  }
+  
+  
+  dist.classes <- levels(cut(distData[, distCol], dist.breaks, 
+                             include.lowest = TRUE))
+  ya <- array(NA, c(M, J, T), dimnames = list(levels(transects), 
+                                              dist.classes, paste("rep", 1:T, sep = "")))
+  transect.levels <- levels(transects)
+  occasion.levels <- levels(occasions)
+  for (i in 1:M) {
+    for (t in 1:T) {
+      sub <- distData[transects == transect.levels[i] & 
+                        occasions == occasion.levels[t], , drop = FALSE]
+      ya[i, , t] <- table(cut(sub[, distCol], dist.breaks, 
+                              include.lowest = TRUE))
     }
-    else {
-        occasions <- distData[,occasionCol]
-        if(!is.factor(occasions)) {
-            occasions <- as.factor(occasions)
-            warning("The occasions were converted to a factor")
-        }
-        T <- nlevels(occasions)
-    }
-    M <- nlevels(transects)
-    J <- length(dist.breaks) - 1
-    dist.classes <- levels(cut(distData[,distCol], dist.breaks,
-                               include.lowest=TRUE))
-    ya <- array(NA, c(M, J, T),
-                dimnames = list(levels(transects),
-                                dist.classes,
-                                paste("rep", 1:T, sep="")))
-    transect.levels <- levels(transects)
-    occasion.levels <- levels(occasions)
-    for(i in 1:M) {
-        for(t in 1:T) {
-            sub <- distData[transects==transect.levels[i] &
-                            occasions==occasion.levels[t],,drop=FALSE]
-            ya[i,,t] <- table(cut(sub[,distCol], dist.breaks,
-                                  include.lowest=TRUE))
-        }
-    }
-    y <- matrix(ya, nrow=M, ncol=J*T)
-    dn <- dimnames(ya)
-    rownames(y) <- dn[[1]]
-    if(T==1)
-        colnames(y) <- dn[[2]]
-    else
-        colnames(y) <- paste(rep(dn[[2]],times=T), rep(1:T, each=J), sep="")
-    return(y)
+  }
+  y <- matrix(ya, nrow = M, ncol = J * T)
+  # takes into account the effortMatrix to allow for the insertion of NAs instead of 0s for surveys which were not completed  
+  ee <- array(NA, c(M,length(occasion.levels)*(length(dist.breaks)-1)))
+  for(i in 1:length(occasion.levels)){
+    ee[,((ncol(ee)/length(occasion.levels)*(i-1)+1):(ncol(ee)/length(occasion.levels)*i))] <- matrix(effortMatrix[,i], ncol=J, nrow=M)
+  }
+  ee[ee==0] <- NA
+  y <- y * ee
+  dn <- dimnames(ya)
+  rownames(y) <- dn[[1]]
+  if (T == 1) 
+    colnames(y) <- dn[[2]]
+  else colnames(y) <- paste(rep(dn[[2]], times = T), rep(1:T, each = J), sep = "")
+  return(y)
 }
-
 
 
 ## Sight distance to perpendicular distance
