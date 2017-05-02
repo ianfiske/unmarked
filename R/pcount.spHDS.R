707dc65aa969020019bb797a91451c2804bee8d9
unknown <jaroyle@gmail.com>
2017-05-01 22:19:11 -0400

    fixes to pcount.spHDS stuff

--- R/pcount.spHDS.R
+++ R/pcount.spHDS.R
@@ -5,22 +5,27 @@
 
 # Function fits spatial hierarchical distance sampling model
 # unmarked model fitting function introduced in Section 9.8.4)
+# update 4/28/2017
 
-pcount.spHDS <- function (formula, data, K, mixture = c("P", "NB", "ZIP"), starts,
-    method = "BFGS", se = TRUE, engine = c("C", "R"), ...)
+pcount.spHDS<-
+function (formula, data, K, mixture = c("P", "NB", "ZIP"), starts,
+    method = "BFGS", se = TRUE, ...)
 {
     mixture <- match.arg(mixture, c("P", "NB", "ZIP"))
     if (!is(data, "unmarkedFramePCount"))
         stop("Data is not an unmarkedFramePCount object.")
-    engine <- match.arg(engine, c("C", "R"))
+   engine<- "R"
+   #engine <- match.arg(engine, c("C", "R"))
     if (identical(mixture, "ZIP") & identical(engine, "R"))
         stop("ZIP mixture not available for engine='R'")
-    designMats <- getDesign(data, formula)
+    designMats <- unmarked:::getDesign(data, formula)
+
     X <- designMats$X
     V <- designMats$V
     y <- designMats$y
     X.offset <- designMats$X.offset
     V.offset <- designMats$V.offset
+
     if (is.null(X.offset))
         X.offset <- rep(0, nrow(X))
     if (is.null(V.offset))
@@ -49,23 +54,29 @@ pcount.spHDS <- function (formula, data, K, mixture = c("P", "NB", "ZIP"), start
         stop(paste("The number of starting values should be",
             nP))
     if (identical(engine, "R")) {
+
         y.ij <- as.numeric(t(y))
         y.ijk <- rep(y.ij, each = K + 1)
         navec <- is.na(y.ijk)
         ijk <- expand.grid(k = 0:K, j = 1:J, i = 1:M)
         ijk.to.ikj <- with(ijk, order(i, k, j))
         nll <- function(parms) {
+            # Force the scale parameter to be positive. This is 1/(2*sigma*sigma)
+            tmp.parms<- parms[(nAP + 1):(nAP + nDP)]
+            tmp.parms[1]<- -1*exp(tmp.parms[1])
+            parms[(nAP + 1):(nAP + nDP)]<- tmp.parms
+
             theta.i <- exp(X %*% parms[1:nAP] + X.offset)
-            p.ij <- 2*plogis(V %*% parms[(nAP + 1):(nAP + nDP)] +
+            p.ij <- 2*plogis(V %*% (parms[(nAP + 1):(nAP + nDP)]) +
                 V.offset)
             theta.ik <- rep(theta.i, each = K + 1)
             p.ijk <- rep(p.ij, each = K + 1)
             bin.ijk <- dbinom(y.ijk, k.ijk, p.ijk)
+  #          if(any(is.nan(bin.ijk))) browser()
             bin.ijk[which(is.na(bin.ijk))] <- 1
             bin.ik.mat <- matrix(bin.ijk[ijk.to.ikj], M * (K +
                 1), J, byrow = TRUE)
-            # g.ik <- rowProds(bin.ik.mat)
-            g.ik <- apply(bin.ik.mat, 1, prod)
+            g.ik <- rowProds(bin.ik.mat)
             if (identical(mixture, "P")) {
                 f.ik <- dpois(k.ik, theta.ik)
             }
@@ -103,22 +114,22 @@ pcount.spHDS <- function (formula, data, K, mixture = c("P", "NB", "ZIP"), start
     }
     fmAIC <- 2 * fm$value + 2 * nP
     stateName <- "Abundance"
-    stateEstimates <- unmarkedEstimate(name = stateName, short.name = "lam",
+    stateEstimates <- unmarked:::unmarkedEstimate(name = stateName, short.name = "lam",
         estimates = ests[1:nAP], covMat = as.matrix(covMat[1:nAP,
             1:nAP]), invlink = "exp", invlinkGrad = "exp")
-    detEstimates <- unmarkedEstimate(name = "Detection", short.name = "p",
+    detEstimates <- unmarked:::unmarkedEstimate(name = "Detection", short.name = "p",
         estimates = ests[(nAP + 1):(nAP + nDP)], covMat = as.matrix(covMat[(nAP +
             1):(nAP + nDP), (nAP + 1):(nAP + nDP)]), invlink = "logistic",
         invlinkGrad = "logistic.grad")
-    estimateList <- unmarkedEstimateList(list(state = stateEstimates,
+    estimateList <- unmarked:::unmarkedEstimateList(list(state = stateEstimates,
         det = detEstimates))
     if (identical(mixture, "NB")) {
-        estimateList@estimates$alpha <- unmarkedEstimate(name = "Dispersion",
+        estimateList@estimates$alpha <- unmarked:::unmarkedEstimate(name = "Dispersion",
             short.name = "alpha", estimates = ests[nP], covMat = as.matrix(covMat[nP,
                 nP]), invlink = "exp", invlinkGrad = "exp")
     }
     if (identical(mixture, "ZIP")) {
-        estimateList@estimates$psi <- unmarkedEstimate(name = "Zero-inflation",
+        estimateList@estimates$psi <- unmarked:::unmarkedEstimate(name = "Zero-inflation",
             short.name = "psi", estimates = ests[nP], covMat = as.matrix(covMat[nP,
                 nP]), invlink = "logistic", invlinkGrad = "logistic.grad")
     }
