8ffb655e87f9b2de3f1d94be3791bee1c3fda100
Ken Kellner <ken@kenkellner.com>
2020-06-02 11:15:54 -0400

    Convert characters to factors with warning when building umf

--- inst/unitTests/runit.unmarkedFrame.R
+++ inst/unitTests/runit.unmarkedFrame.R
@@ -102,3 +102,75 @@ test.umf.yearlySiteCovs <- function() {
   checkEqualsNumeric(dim(as_df2),c(50,25))
 
 }
+
+test.umf.char.to.factor <- function(){
+
+  n <- 50   # number of sites
+  T <- 4    # number of primary periods
+  J <- 3    # number of secondary periods
+
+  y <- matrix(0:1, n, J*T)
+
+  #Site covs
+  sc <- data.frame(x=rnorm(n), y=sample(letters, 50, replace=TRUE))
+  checkEquals(sapply(sc, class), c(x="numeric", y="character"))
+  
+  options(warn=2)
+  checkException(umf <- unmarkedFrame(y, siteCovs=sc))
+  options(warn=0)
+  umf <- unmarkedFrame(y, siteCovs=sc)
+  checkEquals(sapply(siteCovs(umf), class), c(x="numeric", y="factor"))
+
+  #Already factor
+  sc2 <- data.frame(x=rnorm(n), y=factor(sample(letters, 50, replace=TRUE)))
+  umf <- unmarkedFrame(y, siteCovs=sc2)
+  checkEquals(sapply(siteCovs(umf), class), c(x="numeric", y="factor"))
+
+  #Obs covs
+  oc <- data.frame(x=rnorm(n*J*T), y=sample(letters, n*J*T, replace=TRUE))
+  checkEquals(sapply(oc, class), c(x="numeric", y="character"))
+  
+  options(warn=2)
+  checkException(umf <- unmarkedFrame(y, obsCovs=oc))
+  options(warn=0)
+  umf <- unmarkedFrame(y, obsCovs=oc)
+  checkEquals(sapply(obsCovs(umf), class), c(x="numeric", y="factor"))
+  checkTrue(is.null(siteCovs(umf)))
+
+  #as list
+  oc <- list(x=matrix(oc$x, nrow=n), y=matrix(oc$y, nrow=n))
+  options(warn=2)
+  checkException(umf <- unmarkedFrameOccu(y, obsCovs=oc))
+  options(warn=0)
+  umf <- unmarkedFrameOccu(y, obsCovs=oc)
+  checkEquals(sapply(obsCovs(umf), class), c(x="numeric", y="factor"))
+  checkTrue(is.null(siteCovs(umf)))
+  
+  #Check conversion
+  df <- as(umf, "data.frame")
+  checkEqualsNumeric(dim(df), c(50,36))
+
+  #Yearly site covs
+  ysc <- list(x=matrix(rnorm(n*T), nrow=n), 
+             y=matrix(sample(letters, n*T, replace=TRUE), nrow=n))
+  options(warn=2)
+  checkException(umf <- unmarkedMultFrame(y, yearlySiteCovs=ysc, numPrimary=T))
+  options(warn=0)
+  umf <- unmarkedMultFrame(y, yearlySiteCovs=ysc, numPrimary=T)
+  checkEquals(sapply(yearlySiteCovs(umf), class), c(x="numeric", y="factor"))
+  checkTrue(is.null(siteCovs(umf)))
+
+  #All
+  options(warn=2)
+  checkException(umf <- unmarkedMultFrame(y, yearlySiteCovs=ysc, obsCovs=oc, 
+                                          siteCovs=sc, numPrimary=T))
+  options(warn=0)
+  umf <- unmarkedMultFrame(y, yearlySiteCovs=ysc, obsCovs=oc, 
+                          siteCovs=sc, numPrimary=T)
+  checkEquals(sapply(yearlySiteCovs(umf), class), c(x="numeric", y="factor"))
+  checkEquals(sapply(obsCovs(umf), class), c(x="numeric", y="factor"))
+  checkEquals(sapply(obsCovs(umf), class), c(x="numeric", y="factor"))
+  
+  df <- as(umf, "data.frame")
+  checkEqualsNumeric(dim(df), c(50,46)) 
+}
