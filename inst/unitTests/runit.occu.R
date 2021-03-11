935df2c334a6af3b0a574a349987f7c2f1ae8d1a
Ken Kellner <ken@kenkellner.com>
2021-03-11 13:11:23 -0500

    Add occu random effect tests

--- inst/unitTests/runit.occu.R
+++ inst/unitTests/runit.occu.R
@@ -1,5 +1,5 @@
 test.occu.fit.simple.1 <- function() {
-  
+
   y <- matrix(rep(1,10),5,2)
   umf <- unmarkedFrameOccu(y = y)
   fm <- occu(~ 1 ~ 1, data = umf)
@@ -55,7 +55,7 @@ test.occu.fit.covs <- function() {
   obsCovs <- data.frame(o1 = 1:10, o2 = exp(-5:4)/10)
   umf <- unmarkedFrameOccu(y = y, siteCovs = siteCovs, obsCovs = obsCovs)
   fm <- occu(~ o1 + o2 ~ x, data = umf)
-  
+
   occ <- fm['state']
   det <- fm['det']
 
@@ -66,7 +66,7 @@ test.occu.fit.covs <- function() {
 
   occ.lc <- linearComb(fm, type = 'state', c(1, 0.5))
   det.lc <- linearComb(fm, type = 'det', c(1, 0.3, -0.3))
-    
+
   checkEqualsNumeric(coef(occ.lc), 9.826848, tol = 1e-4)
   checkEqualsNumeric(coef(det.lc), 0.2681477, tol = 1e-4)
 
@@ -77,7 +77,7 @@ test.occu.fit.covs <- function() {
   checkException(backTransform(fm, type = "det"))
 
   fitted <- fitted(fm)
-  checkEqualsNumeric(fitted, structure(c(0.5738, 0.5014, 0.4318, 0.38581, 0.50171, 0.53764, 
+  checkEqualsNumeric(fitted, structure(c(0.5738, 0.5014, 0.4318, 0.38581, 0.50171, 0.53764,
 0.46563, 0.40283, 0.39986, 0.79928), .Dim = c(5L, 2L)), tol = 1e-5)
 
 }
@@ -127,22 +127,22 @@ test.occu.offest <- function() {
   umf <- unmarkedFrameOccu(y = y, siteCovs = siteCovs, obsCovs = obsCovs)
   fm <- occu(~ o1 + o2 ~ offset(x), data = umf)
   checkEqualsNumeric(coef(fm),
-                     structure(c(9.74361, 0.44327, -0.14683, 0.44085), .Names = c("psi(Int)", 
+                     structure(c(9.74361, 0.44327, -0.14683, 0.44085), .Names = c("psi(Int)",
 "p(Int)", "p(o1)", "p(o2)")), tol = 1e-5)
   fm <- occu(~ o1 + offset(o2) ~ offset(x), data = umf)
-  checkEqualsNumeric(coef(fm), structure(c(8.59459, 0.97574, -0.3096), .Names = c("psi(Int)", 
+  checkEqualsNumeric(coef(fm), structure(c(8.59459, 0.97574, -0.3096), .Names = c("psi(Int)",
 "p(Int)", "p(o1)")), tol=1e-5)
 
 }
 
 test.occu.cloglog <- function() {
-  
+
   #Adapted from example by J. Cohen
   set.seed(123)
-  M = 500 #sample size 
+  M = 500 #sample size
   J = 3 #number of visits
 
-  #standardized covariates 
+  #standardized covariates
   elev <- runif(n = M, 0,100)
   forest <- runif(n = M, 0,1)
   wind <- array(runif(n = M * J, 0,20), dim = c(M, J))
@@ -151,28 +151,28 @@ test.occu.cloglog <- function() {
   wind[,1] <- as.numeric(scale(wind[,1]))
   wind[,2] <- as.numeric(scale(wind[,2]))
   wind[,3] <- as.numeric(scale(wind[,3]))
-   
+
   #regression parameters for abundance
   beta0 = -0.69
-  beta1 = 0.71 
+  beta1 = 0.71
   beta2 = -0.5
 
   #simulate abundance and derive true occupancy
   lambda <- exp(beta0 + beta1 * elev + beta2 * forest)
   N <- rpois(n = M, lambda = lambda)
   z <- as.numeric(N>0)
-  #regression parameters for detection 
+  #regression parameters for detection
   alpha0 = -0.84
   alpha1 = 2.
   alpha2 = -1.2
-  
+
   #simulate detection
   p <- plogis(alpha0 + alpha1 * elev + alpha2 * wind )
-  
+
   #create vectors of simulation values, for easy comparison to model estimates
   true.beta.p <- c(alpha0,alpha1,alpha2)
   true.beta.occ <- c(beta0,beta1,beta2)
- 
+
   #generate observed presence
   Obs.pres <- matrix(NA,M,J)
   for (i in 1:M){
@@ -182,7 +182,7 @@ test.occu.cloglog <- function() {
   }
   Obs.ever <- apply(Obs.pres,1,max)
 
-  #create observation-level covariate data frame for unmarked  
+  #create observation-level covariate data frame for unmarked
   sitevec <- rep(1:M,3) #vector of site ID's
   wind.df <- data.frame("wind"=wind)
   colnames(wind.df) <- c("Wind.1","Wind.2","Wind.3")
@@ -195,21 +195,21 @@ test.occu.cloglog <- function() {
   #create unmarked data object
   occ.frame <- unmarkedFrameOccu(Obs.pres,
                                  siteCovs=data.frame("ele"=elev,"forest"=forest),
-                                  obsCovs=wind.for.um) 
+                                  obsCovs=wind.for.um)
 
   #create model object
   occ_test <-occu(~ele+wind ~ele+forest, occ.frame, linkPsi="cloglog",
-                      se=F)  
+                      se=F)
   truth <- c(true.beta.occ, true.beta.p)
   est <- coef(occ_test)
   checkEqualsNumeric(truth, est, tol=0.1)
-  checkEqualsNumeric(est, 
+  checkEqualsNumeric(est,
     c(-0.7425,0.6600,-0.3333,-0.87547,2.0677,-1.3082), tol=1e-4)
-  
+
   est_obj <- occ_test@estimates@estimates$state
   checkEquals(est_obj@invlink, "cloglog")
   checkEquals(est_obj@invlinkGrad, "cloglog.grad")
-  
+
   #Check error if wrong link function
   checkException(occu(~ele+wind ~ele+forest, occ.frame, linkPsi="fake"))
 }
@@ -221,13 +221,13 @@ test.occu.predict.complexFormulas <- function() {
   obsCovs <- data.frame(o1 = 1:10, o2 = exp(-5:4)/10)
   umf <- unmarkedFrameOccu(y = y, siteCovs = siteCovs, obsCovs = obsCovs)
   fm <- occu(~ scale(o1) + o2 ~ x, data = umf)
-  
+
   #Predict values should not depend on means/variance of newdata itself
   nd1 <- obsCovs(umf[1:2,])
   pr1 <- predict(fm, 'det', newdata=nd1)
   nd2 <- obsCovs(umf[1:4,])
   pr2 <- predict(fm, 'det', newdata=nd2)[1:4,]
-  
+
   checkEqualsNumeric(pr1, pr2)
 
   #Check factors
@@ -265,10 +265,52 @@ test.occu.offest <- function() {
   umf <- unmarkedFrameOccu(y = y, siteCovs = siteCovs, obsCovs = obsCovs)
   fm <- occu(~ o1 + o2 ~ offset(x), data = umf)
   checkEqualsNumeric(coef(fm),
-                     structure(c(9.74361, 0.44327, -0.14683, 0.44085), .Names = c("psi(Int)", 
+                     structure(c(9.74361, 0.44327, -0.14683, 0.44085), .Names = c("psi(Int)",
 "p(Int)", "p(o1)", "p(o2)")), tol = 1e-5)
   fm <- occu(~ o1 + offset(o2) ~ offset(x), data = umf)
-  checkEqualsNumeric(coef(fm), structure(c(8.59459, 0.97574, -0.3096), .Names = c("psi(Int)", 
+  checkEqualsNumeric(coef(fm), structure(c(8.59459, 0.97574, -0.3096), .Names = c("psi(Int)",
 "p(Int)", "p(o1)")), tol=1e-5)
 
+}
+
+test.occu.randomeffects <- function(){
+  set.seed(123)
+  n_sites <- 100
+  n_years <- 8
+  site_id <- rep(1:n_sites, each=n_years)
+  M <- n_sites * n_years
+  J <- 5 # number of obs per year
+  site_covs <- data.frame(cov1=rnorm(M), site_id=factor(site_id))
+  beta <- c(intercept=0.5, cov1=0.3)
+  sig <- 1.2
+  site_effect <- rnorm(n_sites, 0, sig)
+  true_site_means <- plogis(beta[1] + site_effect)
+
+  psi <- rep(NA, M)
+  for (i in 1:M){
+    #Random group intercept on psi
+    psi[i] <- plogis(beta[1] + beta[2]*site_covs$cov1[i]
+                      + site_effect[site_id[i]])
+  }
+
+  p <- 0.5
+  z <- rbinom(M, 1, psi)
+  y <- matrix(0, nrow=M, ncol=J)
+
+  for (i in 1:M){
+    if(z[i]==1){
+      y[i,] <- rbinom(J, 1, p)
+    }
+  }
+
+  umf <- unmarkedFrameOccu(y=y, siteCovs=site_covs)
+  fm <- occu(~1~cov1 + (1|site_id), umf)
+  checkEqualsNumeric(coef(fm), c(0.65293, 0.39965, -0.02822), tol=1e-4)
+  checkEqualsNumeric(sigma(fm)$sigma, 1.18816, tol=1e-4)
+
+  pr <- predict(fm, "state", newdata=data.frame(cov1=0, site_id=factor(1:100)))
+  checkTrue(inherits(pr, "data.frame"))
+
+  ft <- fitted(fm)
+  checkEqualsNumeric(dim(ft), c(n_sites*n_years, J))
 }
